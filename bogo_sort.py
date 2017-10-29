a65d3950c901a27020949f9cb84001ddf75d3c88
unknown <aphelion030@gmail.com>
2017-10-29 16:48:54 +1100

    global variable fix

--- bogo_sort.py
+++ bogo_sort.py
@@ -16,8 +16,8 @@ def check_sort(numbers):
 def main():
     to_sort = []
 
-    for i in range(10):
-        to_sort.append(int(random()*10+1))
+    for i in range(LIST_LENGTH):
+        to_sort.append(int(random()*NUM_VARIETY+1))
     print("Unsorted: %s\n" % (to_sort))
     
     loops = 0
